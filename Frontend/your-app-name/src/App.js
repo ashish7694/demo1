a7aa65e828fb8f97793ecd4a4bad42cc2994dcb2
ashish <ashish7694@gmail.com>
2024-02-12 12:29:04 +0530

    'fstcom5'

--- Frontend/your-app-name/src/App.js
+++ Frontend/your-app-name/src/App.js
@@ -31,7 +31,11 @@ function App() {
         <p>jjjjjjjjjjjjjjjjjjj</p>
         <p>jjjjjjjjjjjjjjjjjjj</p>
         <p>delhiiiiiiiiiiii</p>    
+<<<<<<< HEAD
 
+=======
+        <p>delhiiiiiiiiiiii</p>    
+>>>>>>> parent of 9fe5bc5 ('fstcom2')
       </header>
     </div>
   );
